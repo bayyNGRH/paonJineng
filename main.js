707a6bdcc60137d33cbdfbad1ea788c77d2e6b78
Bayu Nugraha <110885794+bayyNGRH@users.noreply.github.com>
2023-02-24 19:22:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+// // form js
+// const form = document.getElementById("form");
+// const result = document.getElementById("result");
+
+// form.addEventListener("submit", function (e) {
+//   const formData = new FormData(form);
+//   e.preventDefault();
+//   var object = {};
+//   formData.forEach((value, key) => {
+//     object[key] = value;
+//   });
+//   var json = JSON.stringify(object);
+//   result.innerHTML = "Please wait...";
+
+//   fetch("https://api.web3forms.com/submit", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     },
+//     body: json
+//   })
+//     .then(async (response) => {
+//       let json = await response.json();
+//       if (response.status == 200) {
+//         result.innerHTML = json.message;
+//         result.classList.remove("text-gray-500");
+//         result.classList.add("text-green-500");
+//       } else {
+//         console.log(response);
+//         result.innerHTML = json.message;
+//         result.classList.remove("text-gray-500");
+//         result.classList.add("text-red-500");
+//       }
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       result.innerHTML = "Something went wrong!";
+//     })
+//     .then(function () {
+//       form.reset();
+//       setTimeout(() => {
+//         result.style.display = "none";
+//       }, 5000);
+//     });
+// });
+
+//harga js
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".harga h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
